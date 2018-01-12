6b01ba9961601616ef20a04d7be19f59ef9f6136
Steven Arroyave <stevenarroyave@icloud.com>
2018-01-12 09:36:12 +0000

    Commit
    Commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+// "live" endpoint - request the most recent exchange rate data
+
+https://api.fixer.io/latest
+
+    ? access_key = 34db5cae6cb53c49a14289ebf836ab99
+    & source = GBP
+    & currencies = USD,AUD,CAD,PLN,MXN
+    & format = 1
+
+// Johan Steven, click on the URL above to get the most recent exchange
+// rates for USD, AUD, CAD, PLN and MXN
